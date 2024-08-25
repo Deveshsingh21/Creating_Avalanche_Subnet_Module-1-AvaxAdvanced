df49d30815964024ef775470c50e0b84a49e843e
Deveshsingh21 <deveshsingh5603@gmail.com>
2024-08-25 17:04:07 +0530

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -7,8 +7,8 @@ contract ERC20 {
     mapping(address => uint) public balanceOf;
     mapping(address => mapping(address => uint)) public allowance; 
     
-    string public name = "devennet";
-    string public symbol = "DVNT";
+    string public name = "fizznet";
+    string public symbol = "FZNT";
     uint8 public decimals = 18;
 
 	event Transfer(address indexed from, address indexed to, uint value);
